62e330783af22279a2b6674590666ebdf11565fe
Tyna1288 <121522893+Tyna1288@users.noreply.github.com>
2023-09-09 08:09:31 -0500

    Update items.js
    updated changes in the items.js file

--- routes/items.js
+++ routes/items.js
@@ -1,21 +1,56 @@
 
 
+
 const express = require("express");
+const ExpressError = require("../expressError");
 const router = new express.Router();
 
-const users = [];
 
-/** GET /users: get list of users */
+const items = [];
+
+/** GET /items: get list of items */
 
 router.get("/", function(req, res) {
-  return res.json(users);
+  return res.json(items);
 });
 
-/** DELETE /users/[id]: delete user, return status */
+/** POST /items: add to the list of items */
 
-router.delete("/:id", function(req, res) {
-  const idx = users.findIndex(u => u.id === +req.params.id);
-  users.splice(idx, 1);
+router.post("/", function (req, res) {
+  const newItem = { name: req.body.name.price }
+  items.push(newItem)
+  return res.status(201).json({ item: newItem })
+});
+
+/** GET /items: display single item name and price */
+
+router.get("/:name", function (req, res) {
+  const foundItem = items.find(item => item.name.price === req.params.name.price)
+  if (foundItem === undefined) {
+    throw new ExpressError("Item not found", 404)
+  }
+  return res.json({ item: foundItem });
+});
+
+/** PATCH./items: modify single item name and price */
+
+router.patch("/:name", function (req, res) {
+  const foundItem = items.find(item => item.name.price === req.params.name.price)
+  if (foundItem === undefined) {
+    throw new ExpressError("Item not found, 404")
+  }
+  foundItem.name.price = req.body.name.price
+  return res.json({ item: foundItem });
+});
+
+/** DELETE /items/[name]: delete item, return status */
+
+router.delete("/:name", function(req, res) {
+  const foundItem = items.findIndex(item => item.name === +req.params.name.price);
+  if (foundItem === -1) {
+    throw new ExpressError("Item not found, 404")
+  }
+  items.splice(foundItem, 1);
   return res.json({ message: "Deleted" });
 });
 
