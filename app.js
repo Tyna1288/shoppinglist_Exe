5e377e8e2912fd8426d80208967d661f620e3fa6
Tyna1288 <aaliyah55j@gmail.com>
2023-09-09 06:06:58 -0500

    shopping_list

---
+++ app.js
@@ -0,0 +1,36 @@
+
+const express = require("express")
+const ExpressError = require("./expressError")
+const app = express();
+const itemsRoutes = require("./routes/items")
+
+
+app.use(express.json());
+app.use("/items", itemsRoutes);
+
+/** 404 handler */
+
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
+
+module.exports = app
+
+
+
+
+// end generic handler
+app.listen(3000, function() {
+  console.log("Server is listening on port 3000");
+});
+// end app.listen
